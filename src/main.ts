5c83edb69b4de631e3ed55ecc3006c32ebc762d0
Veri5ied <alvinuchenna14@gmail.com>
2024-10-04 06:52:07 +0100

    WIP: swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,15 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Portixel API')
+    .setDescription('AI-powered portfolio creation and workflow ease')
+    .setVersion('1.0')
+    .addTag('portixel', 'portixel backend')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(4242);
 }
 bootstrap();
