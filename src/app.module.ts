ea1a2ab5a89693282c3377b67359fca78a41a368
Alvin Okoro <43749581+Veri5ied@users.noreply.github.com>
2024-10-07 11:57:54 +0100

    Revert "Ft db models"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { WaitlistModule } from './waitlist/waitlist.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    PrismaModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    WaitlistModule,
-  ],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
