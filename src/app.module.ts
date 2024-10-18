474cde94a3be8540a3eb8a784cdb45258fb19f44
Veri5ied <alvinuchenna14@gmail.com>
2024-10-18 10:29:03 +0100

    WIP: setup app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,20 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { WaitlistModule } from './waitlist/waitlist.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { TemplateModule } from './template/template.module';
+import { IntegrationModule } from './integration/integration.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
     WaitlistModule,
+    AuthModule,
+    TemplateModule,
+    IntegrationModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
