eaeb3ba87dcfb15ac623907c718b686c9a024ee9
Veri5ied <alvinuchenna14@gmail.com>
2024-10-19 23:33:13 +0100

    WIP: migration

--- prisma/migrations/20241019223249_new_migration/migration.sql
+++ prisma/migrations/20241019223249_new_migration/migration.sql
@@ -5,6 +5,17 @@ CREATE TYPE "TypeOfLogin" AS ENUM ('WALLET', 'PLAIN', 'GOOGLE');
 CREATE TYPE "AccountType" AS ENUM ('INDIVIDUAL', 'AGENCY');
 
 -- CreateTable
+CREATE TABLE "WaitList" (
+    "id" SERIAL NOT NULL,
+    "wallet_address" TEXT,
+    "email_address" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "WaitList_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "name" TEXT,
@@ -37,9 +48,16 @@ CREATE TABLE "Integration" (
 CREATE TABLE "Template" (
     "id" SERIAL NOT NULL,
     "template_key" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
 
     CONSTRAINT "Template_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WaitList_wallet_address_key" ON "WaitList"("wallet_address");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WaitList_email_address_key" ON "WaitList"("email_address");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_address_key" ON "User"("email_address");
@@ -49,3 +67,6 @@ CREATE UNIQUE INDEX "User_wallet_address_key" ON "User"("wallet_address");
 
 -- AddForeignKey
 ALTER TABLE "Integration" ADD CONSTRAINT "Integration_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Template" ADD CONSTRAINT "Template_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
