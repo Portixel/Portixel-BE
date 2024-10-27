9c2ebfbe4dab84a2e5a7ff954f6d414f84440bde
Veri5ied <alvinuchenna14@gmail.com>
2024-10-27 14:12:48 +0100

    WIP: figma integrations

--- src/figma-integration/dto/figma.dto.ts
+++ src/figma-integration/dto/figma.dto.ts
@@ -1,10 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class FigmaDto {
+  @ApiProperty({
+    type: String,
+  })
   @IsString()
   @IsNotEmpty()
   code: string;
 
+  @ApiProperty({
+    type: String,
+  })
   @IsString()
   @IsNotEmpty()
   state: string;
